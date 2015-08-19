4ab8bebe37c749448925957748171df2e5f3386a
jinglion <cjhjinglion0529@163.com>
2015-08-19 16:06:15 +0800

    新建一个AA消费助手工程。

---
+++ app/src/main/java/com/example/jinglion/aacharge/Activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.jinglion.aacharge.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.jinglion.aacharge.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
